7307c76d17891c60d228ee64b9e4f9d8d8bfd811
joaovitor3 <joaovytor0@gmail.com>
2021-09-25 12:42:30 -0300

    Add command to dump playlist data to db

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Track(models.Model):
+    name = models.CharField(max_length=100)
+    artists = models.CharField(max_length=100)
+    album_name = models.CharField(max_length=100)
+    album_image = models.CharField(max_length=100)
+    playlists = models.ManyToManyField(Playlist)
+
+    def __str__(self):
+        return self.name
--- playlists/spotify_client.py
+++ playlists/spotify_client.py
@@ -1,28 +1,42 @@
+import logging
+
+from django.conf import settings
+
 import requests
-import os
+
+
+logger = logging.getLogger(__name__)
 
 
 class SpotifyClient(object):
     def __init__(self) -> None:
         self.HOST = "https://api.spotify.com/v1"
-        self.ACCESS_TOKEN = os.getenv("SPOTIFY_ACCESS_TOKEN", "")
+        self.ACCESS_TOKEN = settings.SPOTIFY_ACCESS_TOKEN
         self.HEADERS = {
             "Authorization": f"Bearer {self.ACCESS_TOKEN}"
         }
 
-    def get_playlist(self, playlist_id: str) -> dict:
+    def get_playlist(self, playlist_id: str = None) -> dict:
         """Returns a playlist data"""
+        print('playlist id:', playlist_id)
+        playlist_id = playlist_id if playlist_id else settings.DEFAULT_PLAYLIST_ID
+
         url = f"{self.HOST}/playlists/{playlist_id}"
         response = requests.get(url, headers=self.HEADERS)
         response_json = response.json()
-        return response_json
+        if "error" in response_json:
+            logger.error(
+                f"Error fetching playlist {playlist_id}",
+                extra={"response_json": response_json, "status_code": response.status_code}
+            )
+            return {}
+        else:
+            return response_json
 
     def format_playlist_tracks(self, playlist_data: dict) -> list:
         """Format playlist with data that will be displayed"""
         playlist_tracks = playlist_data["tracks"]["items"]
-        IMAGE_DIMENSION_640 = 0
         IMAGE_DIMENSION_300 = 1
-        IMAGE_DIMENSION_64 = 2
         formatted_playlist_tracks = []
 
         for track in playlist_tracks:
